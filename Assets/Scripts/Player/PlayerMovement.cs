f5e6e875c39c7383f558a9b1aed116f8551d624b
Luqmus <rjcnz1007@yandex.ru>
2023-03-20 16:26:37 +0400

    1

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using DG.Tweening;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private KeyCode _movementKey = KeyCode.Mouse1;
     [SerializeField] private float _speed = 5;
+    [SerializeField] private NavMeshAgent _agent;
 
     private Coroutine _coroutine;
 
@@ -25,59 +27,33 @@ public class PlayerMovement : MonoBehaviour
                     if (_coroutine != null)
                         StopCoroutine(_coroutine);
 
-                    _coroutine = StartCoroutine(Moving1(clickedPoint));
+                    _coroutine = StartCoroutine(Moving(clickedPoint));
                 }
             }
         }
-    }
 
-    private IEnumerator Moving2(Vector3 point)
-    {
-        float turnRate = 0.2f;
-
-        transform.DOLookAt(point, turnRate);
-
-        while (Vector3.Distance(point, transform.position) > 0.1f)
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            GetComponent<CharacterController>().SimpleMove(point);
-            yield return null;
+            GetComponent<Animator>().SetTrigger("Attack");
+            _agent.isStopped = true;
         }
-
-        _coroutine = null;
     }
 
-
-    private IEnumerator Moving1(Vector3 point)
+    private IEnumerator Attacking()
     {
-        GetComponent<Animator>().SetBool("IsRunning", true);
-        float time = Vector3.Distance(point, transform.position) / _speed;
-        float turnRate = 0.2f;
-
-        yield return new WaitForSeconds(0.02f);
-
-        transform.DOLookAt(point, turnRate);
-        transform.DOMove(point, time);
-
-        while (Vector3.Distance(point, transform.position) > 0.1f)
-        {
-            yield return null;
-        }
-
-        GetComponent<Animator>().SetBool("IsRunning", false);
-        _coroutine = null;
+        yield return null;
     }
+
 
     private IEnumerator Moving(Vector3 point)
     {
-        transform.DOLookAt(point, 0.2f);
+        _agent.isStopped = false;
+        _agent.SetDestination(point);
+        GetComponent<Animator>().SetBool("IsRunning", true);
 
+        while (Vector3.Distance(transform.position, point) > _agent.radius)
+            yield return null;
 
-        while (Vector3.Distance(point, transform.position) > 0.1f)
-        {
-            transform.Translate(new Vector3(0, 0, _speed * Time.deltaTime));
-            yield return null; 
-        }
-
-        _coroutine = null;
+        GetComponent<Animator>().SetBool("IsRunning", false);
     }
 }
