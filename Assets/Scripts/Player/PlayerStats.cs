e29421bc5b79cafb201436d4aa0685ee5ec12165
Luqmus <rjcnz1007@yandex.ru>
2023-03-22 17:19:23 +0400

    1

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     public static float MovementSpeed { get; private set; } = 500; // 500 - based
-    public static float AttackSpeed { get; private set; } = 100; // 100 - based
+    public static float AttackSpeed { get; private set; } = 400; // 100 - based
 }
