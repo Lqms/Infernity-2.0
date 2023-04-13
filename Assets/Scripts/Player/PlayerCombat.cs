4afec2918c5f4f32baf71bf8ce43421ed4ba2d06
NomelionYT <106259388+NomelionYT@users.noreply.github.com>
2023-04-13 14:12:18 +0300

    lesson

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -6,10 +6,27 @@ using DG.Tweening;
 public class PlayerCombat : MonoBehaviour
 {
     public bool IsAttacking { get; private set; } = false;
+    public bool IsBlocking { get; private set; } = false;
+    public bool IsCasting { get; private set; } = false;
 
     public void Attack(Animator animator)
     {
         StartCoroutine(Attacking(animator));
+    }
+    
+    public void Cast()
+    {
+        StartCoroutine(Casting());
+    }
+
+    public void Block()
+    {
+        IsBlocking = true; 
+    }
+
+    public void UnBlock()
+    {
+        IsBlocking = false;
     }
 
     private IEnumerator Attacking(Animator animator)
@@ -24,5 +41,14 @@ public class PlayerCombat : MonoBehaviour
         yield return new WaitForSeconds(animationTime); 
 
         IsAttacking = false;
+    }
+    
+    private IEnumerator Casting()
+    {
+        IsCasting = true;
+        
+        yield return new WaitForSeconds(1.94f); 
+
+        IsCasting = false;
     }
 }
